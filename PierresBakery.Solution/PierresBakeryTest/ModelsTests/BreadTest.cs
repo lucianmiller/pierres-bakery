fe4119653dcb3f8322973886f96179813b848e21
Lucian Miller <lucian.miller185@gmail.com>
2022-09-20 13:18:15 -0700

    Add test for AmountBought parameter
    Make sure that amount bought is saving to the parameter of the same name

--- PierresBakery.Solution/PierresBakeryTest/ModelsTests/BreadTest.cs
+++ PierresBakery.Solution/PierresBakeryTest/ModelsTests/BreadTest.cs
@@ -11,8 +11,22 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void BreadConstructor_CreateInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread(0, 0);
+      Bread newBread = new Bread(0);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void GetAmountBought_ReturnsAmountBought_Int()
+    {
+      //Arrange
+      int amount = 10;
+
+      //Act
+      Bread newBread = new Bread(amount);
+      int result = newBread.AmountBought;
+
+      //Assert
+      Assert.AreEqual(amount, result);
     }
   }
 }

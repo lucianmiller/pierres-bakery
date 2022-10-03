75845240f0896c66f05b7628d500dcc0a779d5e8
Lucian Miller <lucian.miller185@gmail.com>
2022-10-03 10:33:54 -0700

    Add test for Pastry constructor

---
+++ PierresBakery.Solution/PierresBakeryTest/ModelsTests/PastryTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class PastryTest
+  {
+    [TestMethod]
+    public void PastryConstructor_CreateInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(0);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}

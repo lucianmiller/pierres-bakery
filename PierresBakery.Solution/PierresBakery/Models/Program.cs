5395a98b49066329685bf9042ee16e44b0e07ae7
Lucian Miller <lucian.miller185@gmail.com>
2022-10-11 09:50:54 -0700

    Fixed wording

--- PierresBakery.Solution/PierresBakery/Models/Program.cs
+++ PierresBakery.Solution/PierresBakery/Models/Program.cs
@@ -6,7 +6,7 @@ public class Program
   public static void Main()
   {
     Console.WriteLine("Welcome to Pierre's Bakery!");
-    Console.WriteLine("How many loafs of bread do you want?");
+    Console.WriteLine("How many loafs of bread do you want? $5 dollars per loaf or buy 2 get the third free!");
     string breadOrder = Console.ReadLine();
     int breadOrderNum = int.Parse(breadOrder);
     Bread newBread = new Bread(breadOrderNum);
@@ -14,7 +14,7 @@ public class Program
     int breadCost = newBread.CurrentPrice;
     Console.WriteLine($"Your amount for bread is ${breadCost}.");
     
-    Console.WriteLine("How many pastries do you want?");
+    Console.WriteLine("How many pastries do you want? Buy 1 for $2 or 3 for $5.");
     string pastryOrder = Console.ReadLine();
     int pastryOrderNum = int.Parse(pastryOrder);
     Pastry newPastry = new Pastry(pastryOrderNum);
